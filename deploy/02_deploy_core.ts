2c0be7d1de3dbbbef8b818f00013ba09d08a2c5d
syuhei176 <syuhei176@gmail.com>
2023-05-16 13:54:59 +0900

    update deploy script

--- deploy/02_deploy_core.ts
+++ deploy/02_deploy_core.ts
@@ -1,6 +1,6 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
-import { networkNameToUSDC, networkNameToWethUniswapPool } from '../tasks/utils'
+import { networkNameToUSDC, networkNameToArbUniswapPool, networkNameToWethUniswapPool } from '../tasks/utils'
 
 const operatorAddress = '0xb8d843c8E6e0E90eD2eDe80550856b64da92ee30'
 const USDC_IRM_PARAMS = {
@@ -15,16 +15,16 @@ const WETH_IRM_PARAMS = {
   slope1: '40000000000000000',
   slope2: '1400000000000000000',
 }
-const PREMIUM_PARAMS = {
-  baseRate: '22000000000000000',
-  kinkRate: '450000000000000000',
-  slope1: '140000000000000000',
-  slope2: '1600000000000000000',
-}
-const ASSET_RISK_PARAMS = {
+
+const ASSET_RISK_PARAMS_MIDDLE = {
   riskRatio: '109544511',
-  rangeSize: 1000,
-  rebalanceThreshold: 500
+  rangeSize: 800,
+  rebalanceThreshold: 400
+}
+const ASSET_RISK_PARAMS_HIGH = {
+  riskRatio: '109544511',
+  rangeSize: 980,
+  rebalanceThreshold: 480
 }
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
@@ -64,12 +64,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
           methodName: 'initialize',
           args: [
             usdc,
-            USDC_IRM_PARAMS,
             [{
               uniswapPool: networkNameToWethUniswapPool(network.name),
-              assetRiskParams: ASSET_RISK_PARAMS,
-              irmParams: WETH_IRM_PARAMS,
-              squartIRMParams: PREMIUM_PARAMS
+              assetRiskParams: ASSET_RISK_PARAMS_MIDDLE,
+              stableIrmParams: USDC_IRM_PARAMS,
+              underlyingIrmParams: WETH_IRM_PARAMS
+            }, {
+              uniswapPool: networkNameToArbUniswapPool(network.name),
+              assetRiskParams: ASSET_RISK_PARAMS_HIGH,
+              stableIrmParams: USDC_IRM_PARAMS,
+              underlyingIrmParams: WETH_IRM_PARAMS
             }]
           ],
         },
@@ -81,7 +85,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const controller = await ethers.getContract('Controller', deployer)
 
     if (network.name === 'arbitrum') {
-      //  await controller.setOperator(operatorAddress)
+      // await controller.setOperator(operatorAddress)
     }
   }
 }
