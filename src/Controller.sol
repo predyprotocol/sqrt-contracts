d8f8ae0b410425fe8de17cdeedb5a606a96c11ef
syuhei176 <syuhei176@gmail.com>
2023-06-08 12:07:14 +0900

    add getLatestAssetId

--- src/Controller.sol
+++ src/Controller.sol
@@ -519,6 +519,12 @@ contract Controller is Initializable, ReentrancyGuard, IUniswapV3MintCallback, I
         return pairs[_id];
     }
 
+    function getLatestAssetStatus(uint256 _id) external returns (DataType.AssetStatus memory) {
+        ApplyInterestLogic.applyInterestForToken(assets, _id);
+
+        return assets[_id];
+    }
+
     function getVault(uint256 _id) external view override(IController) returns (DataType.Vault memory) {
         return vaults[_id];
     }
